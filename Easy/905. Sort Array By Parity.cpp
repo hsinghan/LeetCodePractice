4d466fa5d96c98f17f1ddc8238f804aa5d63a610
Justin <hsinghan1102@gmail.com>
2018-09-20 16:18:20 +0800

    Update 905. Sort Array By Parity.cpp

--- Easy/905. Sort Array By Parity.cpp
+++ Easy/905. Sort Array By Parity.cpp
@@ -24,3 +24,16 @@ public:
         return A;
     }
 };
+
+// Another solution by using the library function in c++
+// partition, which can rearrange the array according to the conddition.
+// It divided the elements into two groups, the elements satisfied the condition be placed in the first group
+//, and the remainder be placed in the second group.
+// The array will be rearranged to the form like this [first group, second group]
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& A) {
+        partition(A.begin(), A.end(), [](int num) {return num%2 == 0;});
+        return A;
+    }
+};
